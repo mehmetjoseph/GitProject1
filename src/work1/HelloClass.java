210ce2d8a1822ae8046135c1570a90a2c1caf7f9
mehmetjoseph <zayiziya34@gmail.com>
2021-06-17 08:20:28 +0200

    Added more work

--- src/work1/HelloClass.java
+++ src/work1/HelloClass.java
@@ -5,6 +5,9 @@ public class HelloClass {
     public static void main(String[] args) {
 
         System.out.println("Work1 started");
+
+        System.out.println("Created Git repository");
+
     }
 
 }
